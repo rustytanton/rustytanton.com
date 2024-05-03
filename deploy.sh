6fdbbea91ad0a25e7d865612aeca341233bb6419
Rusty Tanton <rusty.tanton@gmail.com>
2024-05-03 19:41:25 -0400

    remove deploy.sh

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-# set in Travis environment:
-# $WEBHOST_USER
-# $WEBHOST_DOMAIN
-# $WEBHOST_ROOT
-
-sed -i 's/{{DATE_TODAY}}/$(date +'%b %e, %Y')' ./public_html/index.html
-
-scp -rp ./public_html/* $WEBHOST_USER@$WEBHOST_DOMAIN:$WEBHOST_ROOT/
