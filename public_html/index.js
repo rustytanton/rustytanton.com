924b9ad554abcf4aa3253a4d8b45261a67cfc4cc
Rusty Tanton <rusty.tanton@gmail.com>
2024-05-06 19:34:07 -0400

    big redesign!

---
+++ public_html/index.js
@@ -0,0 +1,23 @@
+function handleWindowResize(elRoot, elAlert) {
+
+    // set a CSS var for the alert height
+    elRoot.style.setProperty('--alert-height', elAlert.offsetHeight + 'px');
+}
+
+
+window.addEventListener('load', () => {
+    // cache elements so we're not querying the DOM over and over
+    const elAlert = document.querySelector('.alert')
+    const elRoot = document.querySelector(':root')
+
+    // only add the event listener if the element exists
+    if (elAlert) {
+        // call on window resize
+        window.addEventListener('resize', () => {
+            handleWindowResize(elRoot, elAlert)
+        })
+
+        // call once on load
+        handleWindowResize(elRoot, elAlert)
+    }
+})
