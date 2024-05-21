868195290547318d0d1090b7c438294d7d1f0b63
Rusty Tanton <rusty.tanton@gmail.com>
2024-05-21 19:04:52 -0400

    cleaner typescript dialog handling

--- public_html/index.ts
+++ public_html/index.ts
@@ -1,3 +1,131 @@
+export const enum htmlClasses {
+  DIALOG = 'dialog',
+  DIALOG_BUTTON_CLOSE = 'dialog-button-close',
+  DIALOG_OPEN = 'dialog-open',
+  DIALOG_PHOTO_LINK = 'dialog-photo-link'
+}
+
+export const enum svgs {
+  DIALOG_CLOSE_BTN = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm4.597 17.954l-4.591-4.55-4.555 4.596-1.405-1.405 4.547-4.592-4.593-4.552 1.405-1.405 4.588 4.543 4.545-4.589 1.416 1.403-4.546 4.587 4.592 4.548-1.403 1.416z"/></svg>'
+}
+
+export interface IElementManager {
+  el: HTMLElement
+  init: () => void
+  addEvents: () => void
+  render: () => void
+}
+
+
+export class ElementManager implements IElementManager {
+  el: HTMLElement
+
+  constructor (el: HTMLElement) {
+    this.el = el
+  }
+
+  init (): void {
+    this.render()
+    this.addEvents()
+  }
+  
+  addEvents(): void {
+    return
+  }
+
+  render(): void {
+    return
+  }
+}
+
+
+export interface IDialog {
+  el: HTMLElement
+  init: () => void
+  open: () => void
+  close: () => void
+}
+
+
+export class Dialog extends ElementManager implements IDialog { 
+  elBtnClose?: HTMLButtonElement
+
+  open(): void {
+    document.body.classList.add(htmlClasses.DIALOG_OPEN);
+    (this.el as HTMLDialogElement).showModal()
+  }
+
+  close(): void {
+    document.body.classList.remove(htmlClasses.DIALOG_OPEN);
+    (this.el as HTMLDialogElement).close()
+  }
+
+  render(): void {
+    this.el.classList.add(htmlClasses.DIALOG)
+
+    this.elBtnClose = document.createElement('button')
+    this.elBtnClose.classList.add(htmlClasses.DIALOG_BUTTON_CLOSE)
+    this.elBtnClose.innerHTML = svgs.DIALOG_CLOSE_BTN
+    this.el.appendChild(this.elBtnClose)
+
+    document.body.appendChild(this.el)
+  }
+
+  addEvents(): void {
+    if (this.elBtnClose) {
+      this.elBtnClose.addEventListener('click', () => {
+        this.close()
+      })
+    }
+    this.el.addEventListener('click', (e: Event) => {
+      if (e.target === this.el) {
+        this.close()
+      }
+    })
+  }
+}
+
+
+export interface IDialogPhotoLink {
+  el: HTMLElement
+  altText: string
+  dialog: IDialog
+  imgSrc: string
+}
+
+
+export class DialogPhotoLink extends ElementManager implements IDialogPhotoLink {
+  altText: string = ''
+  dialog: IDialog
+  imgSrc: string = ''
+
+  constructor (el: HTMLElement, dialog?: IDialog) {
+    super(el)
+    this.dialog = dialog ? dialog : new Dialog(document.createElement('dialog'))
+    this.altText = this.el.getAttribute('data-alt') || ''
+    this.imgSrc = this.el.getAttribute('href') || ''
+  }
+
+  init (): void {
+    this.dialog.init()
+    super.init()
+  }
+
+  addEvents(): void {
+    this.el.addEventListener('click', (e: Event) => {
+      e.preventDefault()
+      this.dialog.open()
+    })
+  }
+
+  render(): void {
+    const elImg = document.createElement('img')
+    elImg.setAttribute('src', this.imgSrc)
+    this.dialog.el.appendChild(elImg)
+  }
+}
+
+
 export async function ready(waitDuration: number = 5000): Promise<string> {
   return new Promise((resolve, reject) => {
     if (document.readyState === 'complete') {
@@ -17,45 +145,6 @@ export async function ready(waitDuration: number = 5000): Promise<string> {
 
 ready().then(() => {
   document.querySelectorAll('.dialog-photo-link').forEach((el) => {
-    const dialog = document.createElement('dialog')
-    dialog.classList.add('dialog-photo')
-    
-    const img = document.createElement('img')
-    img.setAttribute('alt', el.getAttribute('data-alt') || '')
-    img.setAttribute('src', el.getAttribute('href') || '')
-    
-    const btnClose = document.createElement('button')
-    btnClose.classList.add('dialog-photo-button-close')
-    btnClose.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm4.597 17.954l-4.591-4.55-4.555 4.596-1.405-1.405 4.547-4.592-4.593-4.552 1.405-1.405 4.588 4.543 4.545-4.589 1.416 1.403-4.546 4.587 4.592 4.548-1.403 1.416z"/></svg>'
-
-    dialog.appendChild(img)
-    dialog.appendChild(btnClose)
-
-    el.addEventListener('click', (e) => {
-      e.preventDefault()
-      if (dialog.hasAttribute('open')) {
-        document.body.classList.remove('dialog-photo-open')
-        dialog.close()
-      } else {
-        document.body.classList.add('dialog-photo-open')
-        dialog.showModal()
-      }
-    })
-
-    dialog.addEventListener('click', (e) => {
-      const tag = (e.target as HTMLElement)
-      if (tag && tag.tagName.toLowerCase() === 'dialog') {
-        document.body.classList.remove('dialog-photo-open')
-        dialog.close()
-      }
-    })
-
-    btnClose.addEventListener('click', (e) => {
-      document.body.classList.remove('dialog-photo-open')
-      dialog.close()
-    })
-    
-    document.body.appendChild(dialog)
-    console.log(el)
+    new DialogPhotoLink(el as HTMLElement).init()
   })
 })
