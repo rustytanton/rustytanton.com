47e8474c9e42634b6c80a132717476cd61512643
Rusty Tanton <rusty.tanton@gmail.com>
2024-05-21 10:12:45 -0400

    replace sticky js solution with css-only solution

--- public_html/index.ts
+++ public_html/index.ts
@@ -1,58 +1,3 @@
-export const enum htmlClasses {
-  WITH_STICKY_ALERT = 'with-sticky-alert'
-}
-
-export const enum cssVariables {
-  STICKY_ALERT_HEIGHT = '--sticky-alert-height'
-}
-
-export interface IStickyAlert {
-  el: HTMLElement
-  elRoot: HTMLElement
-  addEvents: () => void
-  addAlertClassToBody: () => void
-  setCssVarAlertSize: () => void
-}
-
-export class StickyAlert implements IStickyAlert {
-  el: HTMLElement
-  elRoot: HTMLElement
-
-  constructor (el: HTMLElement, elRoot?: HTMLElement) {
-    this.el = el
-    if (elRoot) {
-      this.elRoot = elRoot
-    } else {
-      this.elRoot = document.querySelector(':root') as HTMLElement
-    }
-  }
-
-  init(): void {
-    this.addEvents()
-    this.setCssVarAlertSize()
-    this.addAlertClassToBody()
-  }
-
-  addEvents(): void {
-    document.addEventListener('resize', () => {
-      this.setCssVarAlertSize()
-    })
-  }
-
-  addAlertClassToBody(): void {
-    document.body.classList.add(htmlClasses.WITH_STICKY_ALERT)
-  }
-
-  setCssVarAlertSize(): void {
-    if (this.el) {
-      this.elRoot.style.setProperty(
-        cssVariables.STICKY_ALERT_HEIGHT,
-        this.el.offsetHeight.toString() + 'px'
-      )
-    }
-  }
-}
-
 export async function ready(waitDuration: number = 5000): Promise<string> {
   return new Promise((resolve, reject) => {
     if (document.readyState === 'complete') {
@@ -70,8 +15,6 @@ export async function ready(waitDuration: number = 5000): Promise<string> {
   })
 }
 
-ready().then(() => {
-  new StickyAlert(
-    document.querySelector('.sticky-alert') as HTMLElement
-  ).init()
-})
+// ready().then(() => {
+  
+// })
