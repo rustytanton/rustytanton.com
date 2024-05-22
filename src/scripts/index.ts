04df6a5eccf5a3eb381cbe3e73044347decba09b
Rusty Tanton <rusty.tanton@gmail.com>
2024-05-22 14:04:01 -0400

    remove console.log

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -3,7 +3,6 @@ import { ready } from './globals/ready'
 import { DialogPhotoLinkView } from './views/DialogPhotoLinkView'
 
 ready().then(() => {
-  console.log(document.readyState)
   document.querySelectorAll('.dialog-photo-link').forEach((el) => {
     new DialogPhotoLinkView(el as HTMLElement).init()
   })
