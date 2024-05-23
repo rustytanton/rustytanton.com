6809ca2b408682f7c45ef45edc1ed7351d67ddd3
Rusty Tanton <rusty.tanton@gmail.com>
2024-05-23 10:20:19 -0400

    add a spec for BaseView coverage

--- src/__tests__/views/BaseView.test.ts
+++ src/__tests__/views/BaseView.test.ts
@@ -15,3 +15,10 @@ test('calls expected methods on init', () => {
     expect(mgr.addEvents).toHaveBeenCalledTimes(1)
     expect(mgr.render).toHaveBeenCalledTimes(1)
 })
+
+test('calls to base addEvents and render methods return undefined', () => {
+    const el = document.createElement('div')
+    const view = new BaseView(el)
+    expect(view.addEvents()).toBe(undefined)
+    expect(view.render()).toBe(undefined)
+})
